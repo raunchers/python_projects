e3cf7ca14c9276db1a2b5369dbd873a0d8ca19fd
Michael Ethridge <maethridge@gmail.com>
2019-08-06 20:30:06 -0400

    Added raw strings

---
+++ strings_lists/raw_string.py
@@ -0,0 +1,13 @@
+# Raw strings, when you do not want python to interpret escape characters
+# Raw strings start with an r
+s = r'This is a raw string, \n escape characters do not work '
+print(s)
+print()
+# Multiline strings use """ """ or ''' '''
+s = """This is
+          a multi-line string using triple double quotes"""
+print(s)
+print()
+s = '''This is a multi-line
+         string using triple single quotes'''
+print(s)
